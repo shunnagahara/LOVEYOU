f3c397d8a69cff9adb7853103b2c24034bf7e8f5
Shun Nagahara <45529007+shunnagahara@users.noreply.github.com>
2021-02-19 00:55:54 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -9,23 +9,66 @@ app.get('/', function(req, res){
     res.sendFile(__dirname + '/public/index.html');
 });
 
+let loginUsers = [];
+
 io.on('connection', function(socket){
 
     let nickName = ""; 
-    let uniqueId = ""; 
+    let existedNameUser = false;
 
+    // 新規接続ユーザー
     socket.on('new comer', function(strNickname_){
+
+      if (existedNameUser) return
+
       nickName = strNickname_;
-      uniqueId = socket.id;
-      socket.broadcast.emit("new comer", nickName, uniqueId);
+      loginUsers.push(nickName);
+
+      socket.broadcast.emit("new comer", nickName);
     });
 
+    // 接続が切れた場合
     socket.on('disconnect',function(){
+
+      if (existedNameUser) return
+
+      var index = loginUsers.indexOf(nickName);
+      if(index >= 0){
+        loginUsers.splice(index, 1);
+      }
+
       socket.broadcast.emit("left", nickName);
     })
 
+    // 同名ユーザーチェック
+    socket.on('check same name', function(myName){
+      if (loginUsers.includes(myName)) existedNameUser = true;
+      io.to(socket.id).emit('check same name', existedNameUser);
+    });
+    
+    // チャットメッセージ
     socket.on('chat message', function(chat){
       io.emit('chat message', chat);
+    });
+    
+    // 告白
+    socket.on('love confession', function(chat){
+
+      if (loginUsers.length < 1) return
+
+      var confessedUsers = loginUsers.concat();
+      var index = confessedUsers.indexOf(nickName);
+
+      if(index >= 0){
+        confessedUsers.splice(index, 1);
+      }
+
+      var confessedUser = confessedUsers[Math.floor(Math.random() * confessedUsers.length)];
+
+      chat.text = confessedUser + 'さんだいすき';
+
+      io.emit('love confession', chat);
+
     });
 });
 
